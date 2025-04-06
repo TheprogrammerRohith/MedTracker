545b06e3f0f16c75728391ab3d901a36f4ef4c3b
Kaustubh-Bharti <kaustubh.bharti2022@vitstudent.ac.in>
2025-04-06 12:05:53 +0530

    changes in profile

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,32 +6,29 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 export default function Header({userName}) {
   const router = useRouter()
   return (
-    <View style={{marginTop:5,padding:25}}>
-        <View
+    <View style={{ marginTop: 5, padding: 25 }}>
+      <View
         style={{
-            display:'flex',
-            flexDirection:'row',
-            gap:15,
-            alignItems:'center'
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between', // evenly space elements
         }}
-        >
-            <Text style={{fontSize:25,fontWeight:'bold'}}>Hello {userName || "UserName"}</Text>
-            <Image source={require('./../assets/images/wave.png')}
-            style={{
-                width:40,
-                height:40,
-            }}
-            />
-            <TouchableOpacity
-            onPress={() => router.push('/add-new-medication')}
-            >
-              <Ionicons name="medkit-outline" size={32} color="black" style={{marginLeft:70}}/>
-              
-            </TouchableOpacity>
-            
+      >
+        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 10 }}>
+          <Text style={{ fontSize: 25, fontWeight: 'bold' }}>
+            Hello {userName || 'UserName'}
+          </Text>
+          <Image
+            source={require('./../assets/images/wave.png')}
+            style={{ width: 40, height: 40 }}
+          />
         </View>
-    </View>
-    
+
+        <TouchableOpacity onPress={() => router.push('/add-new-medication')}>
+          <Ionicons name="medkit-outline" size={32} color="black" />
+        </TouchableOpacity>
+      </View>
+    </View>  
   )
 }
 
